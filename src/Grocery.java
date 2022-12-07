130bd0f51e353b475a130258b1934561c0c71b9e
Angel Chen <angelc470@nycstudents.net>
2022-12-06 21:39:23 -0500

    second commit

--- src/Grocery.java
+++ src/Grocery.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 import java.util.*;
+/**
+ * The Grocery class represents the price of groceries, array lists and has a string name
+ */
+
 public class Grocery {
     private double applePrice;
     private double avoPrice;
@@ -7,18 +11,40 @@ public class Grocery {
     private List<String> shoppingCart = new ArrayList<>();
     private List<Integer> numOfItems = new ArrayList<>();
     private String name;
+
+    /**
+     * Constructor for Grocery class. This initializes the price of each grocery and name
+     */
     public Grocery(){
         applePrice = 0.99;
         avoPrice = 1.10;
         tomatoPrice = 0.5;
         name = "";
     }
+
+    /**
+     * Constructor for Grocery class.
+     *
+     * @param name represents the name that the user inputted
+     */
     public Grocery(String name){
         this.name = name;
     }
+
+    /**
+     * toString method for the Grocery class. This method will return a String greeting the
+     * user using the name they inputted
+     *
+     * @return returns a String in a properly formatted sentence con
+     */
     public String toString(){
         return "Hello, " + name + "!";
     }
+
+    /**
+     * The groceryInto method will introduce the user to the grocery store and the clerk, Alexa.
+     * Alexa will ask the user if they want to see what the store has
+     */
     public void groceryIntro(){
         Scanner scan = new Scanner(System.in);
         System.out.println("-----Welcome to the Grocery Store!-----");
@@ -35,6 +61,12 @@ public class Grocery {
             System.exit(0);
         }
     }
+
+    /**
+     * The whatUserWants method will ask the user what they want and uses while loops to make sure the user inputs a valid input
+     *
+     * @param userWants represents the user's input of what grocery they want from the store
+     */
     public void whatUserWants(String userWants){
         Scanner scan = new Scanner(System.in);
         int count_of_userWants = 1;
@@ -73,6 +105,10 @@ public class Grocery {
         //puts the amount in a list
         numOfItems.add(howManyUserWants);
     }
+
+    /**
+     * The anythingElse method will ask the user if there is anything else the user wants, but the user is unable to put in anything else they want
+     */
     public void anythingElse(){
         Scanner scan = new Scanner(System.in);
         System.out.println("Do you want to buy anything else? (y/n)");
@@ -85,6 +121,12 @@ public class Grocery {
             System.out.println("You entered an invalid input, so I'm just going to take that as a no");
         }
     }
+
+    /**
+     * The Calculate method will calculate the price of the item the user wants multiplied how many the user wants
+     *
+     * @return returns a double of the user's total
+     */
     public double Calculate(){
         //calculates the user's total price
         if((shoppingCart.get(0)).equals("apples")){
@@ -95,9 +137,18 @@ public class Grocery {
             return (double)Math.round(tomatoPrice * numOfItems.get(0) * 100)/100;
         }
     }
+
+    /**
+     * The totalToString method prints a string that tells the user what their total is
+     */
     public void totalToString(){
         System.out.println("Your total is: $" + Calculate() + "\n" + "There's a special event today!");
     }
+
+    /**
+     * The specialEvent method takes user input and checks if the number they inputted is equal to Alexa's number
+     * and if it is, the user gets their groceries for free
+     */
     public void specialEvent(){
         //uses Math.random() for a chance to get free groceries
         Scanner scan = new Scanner(System.in);
